07c2da4472255f529ac1afcfef328c08218fe5d2
Azarattum <Azarattum@users.noreply.github.com>
2018-09-08 14:37:48 +0700

    Structure updated

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	//Init application
-	require_once("libs/Bootstrap.php");
+	require_once("moduls/Bootstrap.php");
 	$application = new Bootstrap();
+	$application->load_libraries("modules");
 	$application->load_libraries("libs");
 ?>
