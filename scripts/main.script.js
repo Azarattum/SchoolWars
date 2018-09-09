0c58cd324e171c730ca94f40b6c07aa0546c21b1
Azarattum <unconfigured@null.spigotmc.org>
2018-09-09 12:46:49 +0700

    Developing main page

--- scripts/main.script.js
+++ scripts/main.script.js
@@ -2,5 +2,8 @@
 /*On page loaded*/
 function main()
 {
-	
+	var swiper = new Swiper(".swiper-container");
+	$(".map-screen-item").bind("touchstart click", function(){swiper.slideTo(0)});
+	$(".profile-screen-item").bind("touchstart click", function(){swiper.slideTo(1)});
+	$(".swiper-container").css("opacity", "1");
 }
